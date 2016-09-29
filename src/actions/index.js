5fad98d0c421caa8ae5fc13a647cb60707194273
Taylor Johnson <taylor.c.johnson@usace.army.mil>
2016-09-29 08:33:35 -0400

    Add explanatory comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+// Actions are javascript objects with a type and payload of state
+// ActionCreators are functions that deliver an action with state for a container
+
 export function selectBook(book) {
   // selectBook is an ActionCreator, return an action - object with type property
   return ({
     type: 'BOOK_SELECTED',
     payload: book
   });
-  //console.log("Selected: " + book.title);
 }
